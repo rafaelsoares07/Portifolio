ec809c2b422cd72df2261a595d0d9ac33f8bc34e
Rafael Soares <rafaelsoares1936@gmail.com>
2022-03-06 19:52:09 -0300

    quase finalizando menu responsivo

---
+++ static/script.js
@@ -0,0 +1,22 @@
+let btn = document.querySelector(".menu-mobile")
+let body = document.querySelector("body")
+let a = document.querySelector("#header")
+let aside  = document.querySelector("aside")
+
+console.log(a)
+
+btn.addEventListener('click',()=>{
+    btn.classList.contains('bi-list')?
+    btn.classList.replace('bi-list', 'bi-x'):
+    btn.classList.replace('bi-x','bi-list');
+
+    body.classList.toggle("menu-nav-active");
+
+    
+
+ 
+
+
+
+})
+
