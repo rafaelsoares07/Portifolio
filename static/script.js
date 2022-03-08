28b95bb5a334396e204f8642eacafc6ef8d7d816
Rafael Soares <rafaelsoares1936@gmail.com>
2022-03-08 14:49:40 -0300

    ANIMACAO DE SCROLL ADICIONADA

--- static/script.js
+++ static/script.js
@@ -4,27 +4,73 @@ let listItem = document.querySelectorAll(".nav-item")
 let header = document.querySelector('#header')
 
 
-
-console.log(header)
-
-btn.addEventListener('click',()=>{
-    btn.classList.contains('bi-list')?
-    btn.classList.replace('bi-list', 'bi-x'):
-    btn.classList.replace('bi-x','bi-list');
+btn.addEventListener('click', () => {
+    btn.classList.contains('bi-list') ?
+        btn.classList.replace('bi-list', 'bi-x') :
+        btn.classList.replace('bi-x', 'bi-list');
 
     body.classList.toggle("menu-nav-active");
 })
 
-listItem.forEach((item)=>{
-    item.addEventListener('click',()=>{
-        if(body.classList.contains('menu-nav-active')){
+listItem.forEach((item) => {
+    item.addEventListener('click', () => {
+        if (body.classList.contains('menu-nav-active')) {
             body.classList.remove('menu-nav-active')
-            btn.classList.replace('bi-x','bi-list')
+            btn.classList.replace('bi-x', 'bi-list')
             header.classList.add('ocultar')
         }
 
 
     })
-    
+
 })
+
+
+//ANIMAÇÕES DAS SECTIONS COM O SCROLL//
+var target = $('.animacao');
+var animationClass = "animacao-play"
+var heigth = $(window).height() * 3 / 5;
+
+
+function animeScroll() {
+    var documetTop = $(document).scrollTop();
+
+    target.each(function () {
+        var itemTop = $(this).offset().top;
+        console.log(itemTop)
+        if (documetTop > itemTop - heigth) {
+            $(this).addClass(animationClass)
+            var a = 5
+
+        }
+        else {
+            $(this).removeClass(animationClass)
+            var b = 6
+        }
+    })
+}
+
+animeScroll();
+
+//EVENTO DE SCROLL QUE ACIONA NOSSA FUNCAO DE ANMAÇÃO//
+$(document).scroll(function () {
+    
+    animeScroll();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
